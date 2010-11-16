63fda5283d5f1c0a75ee0000801404582e90f6d8
Leo D'Angelo <ldangelo@orangeleap.com>
2010-11-16 13:50:52 -0600

    Added Session.java

---
+++ src/main/java/com/orangeleap/webtools/domain/Session.java
@@ -0,0 +1,20 @@
+package com.orangeleap.webtools.domain;
+
+public class Session {
+	String sessionId;
+	Long   constituentId;
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	public Long getConstituentId() {
+		return constituentId;
+	}
+	public void setConstituentId(Long constituentId) {
+		this.constituentId = constituentId;
+	}
+	
+	
+}
