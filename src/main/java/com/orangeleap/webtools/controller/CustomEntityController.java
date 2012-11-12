08edab0eacc11f8dece9c8d6f978e4c53b196db7
alo <alex.lo@orangeleap.com>
2012-11-12 11:08:37 -0600

    TANGERINE-5011: logout is available for Sponsorship & Donation widgets

--- src/main/java/com/orangeleap/webtools/controller/CustomEntityController.java
+++ src/main/java/com/orangeleap/webtools/controller/CustomEntityController.java
@@ -226,7 +226,9 @@ public class CustomEntityController extends MultiActionController {
 		final Map<String, List<Map<String, Object>>> picklistNameItems = widgetService.findPicklistItemsForCustomEntities(ceList, guid);
 		metaData.put("picklistNameItems", picklistNameItems);
 
-		metaData.put("allowLogout", widget.getWidgetAuthenticationRequired());
+		metaData.put("allowLogout", widget.getWidgetAuthenticationRequired() ||
+				"online_sponsorship".equalsIgnoreCase(widget.getCustomEntityName()) ||
+				"online_donation".equalsIgnoreCase(widget.getCustomEntityName()));
 
 		modelMap.put("metaData", metaData);
 		modelMap.put("success", true);
